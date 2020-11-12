3ed09f737be1a78a38c769808f04b5329f4a8d89
thekuldeep07 <thekuldeep07@gmail.com>
2020-11-12 11:59:29 +0530

    some more addition of question model class and question bank class

---
+++ app/src/main/java/com/example/quuizzy/data/QuestionBank.java
@@ -0,0 +1,60 @@
+package com.example.quuizzy.data;
+
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.quuizzy.controller.AppController;
+import com.example.quuizzy.model.Question;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+
+public class QuestionBank {
+
+    ArrayList<Question> questionArrayList = new ArrayList<>();
+    private String url = "https://raw.githubusercontent.com/curiousily/simple-quiz/master/script/statements-data.json";
+    public List<Question> getQuestions(){
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                (JSONArray) null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        for (int i =0 ;i<response.length();i++){
+                            try {
+                                Log.d("array",""+response.getJSONArray(i).get(0));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }
+
+        );
+        AppController.getInstance().addToRequestQueue(jsonArrayRequest);
+
+        return null;
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/quuizzy/model/Question.java
@@ -0,0 +1,30 @@
+package com.example.quuizzy.model;
+
+public class Question {
+    private String answer;
+    private boolean answerTrue;
+
+    public Question(String answer, boolean answerTrue) {
+        this.answer = answer;
+        this.answerTrue = answerTrue;
+    }
+
+    public Question() {
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public boolean isAnswerTrue() {
+        return answerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        this.answerTrue = answerTrue;
+    }
+}
